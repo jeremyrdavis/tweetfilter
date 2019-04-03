e2f3b3f699d8b4842167338756c4d24ea227b3d2
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-04-03 09:22:56 -0400

    splitting and removing commas

--- src/main/java/io/vertx/examples/tweetfilter/MainVerticle.java
+++ src/main/java/io/vertx/examples/tweetfilter/MainVerticle.java
@@ -1,6 +1,7 @@
 package io.vertx.examples.tweetfilter;
 
 import io.reactivex.Flowable;
+import io.reactivex.Observable;
 import io.vertx.core.Future;
 import io.vertx.core.file.OpenOptions;
 import io.vertx.reactivex.FlowableHelper;
@@ -9,17 +10,32 @@ import io.vertx.reactivex.core.buffer.Buffer;
 import io.vertx.reactivex.core.file.AsyncFile;
 import io.vertx.reactivex.core.file.FileSystem;
 
+/**
+ * Thanks to @medal.tv
+ * https://gist.github.com/PimDeWitte
+ * https://gist.github.com/PimDeWitte/c04cc17bc5fa9d7e3aee6670d4105941
+ * for the list of words
+ */
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
 
     FileSystem fileSystem = vertx.fileSystem();
+    fileSystem
+      .rxReadFile("Word_Filter-Sheet1.csv")
+      .toFlowable()
+      .flatMap(buffer -> Flowable.fromArray(buffer.toString().replace(",", "")))
+      .forEach(System.out::println);
+/*
     fileSystem.open("Word_Filter-Sheet1.csv", new OpenOptions(), result -> {
       AsyncFile file = result.result();
-      Flowable<Buffer> observable = file.toFlowable();
-      observable.forEach(data -> System.out.println("Read data: " + data.toString("UTF-8")));
+      Flowable<Buffer> flowable = file.toFlowable();
+      flowable
+        .flatMap(w -> )
+        .forEach(System.out::println);
     });
+*/
     startFuture.complete();
 
   }
