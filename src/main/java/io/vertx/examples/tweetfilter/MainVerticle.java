ba3eac6bec3baf60c464408cf1e09f219e411a4e
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-04-03 09:00:12 -0400

    pulling words from the file

--- src/main/java/io/vertx/examples/tweetfilter/MainVerticle.java
+++ src/main/java/io/vertx/examples/tweetfilter/MainVerticle.java
@@ -1,24 +1,27 @@
 package io.vertx.examples.tweetfilter;
 
-import io.vertx.core.AbstractVerticle;
+import io.reactivex.Flowable;
 import io.vertx.core.Future;
+import io.vertx.core.file.OpenOptions;
+import io.vertx.reactivex.FlowableHelper;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.vertx.reactivex.core.file.AsyncFile;
+import io.vertx.reactivex.core.file.FileSystem;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startFuture.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startFuture.fail(http.cause());
-      }
+
+    FileSystem fileSystem = vertx.fileSystem();
+    fileSystem.open("Word_Filter-Sheet1.csv", new OpenOptions(), result -> {
+      AsyncFile file = result.result();
+      Flowable<Buffer> observable = file.toFlowable();
+      observable.forEach(data -> System.out.println("Read data: " + data.toString("UTF-8")));
     });
+    startFuture.complete();
+
   }
 
 }
