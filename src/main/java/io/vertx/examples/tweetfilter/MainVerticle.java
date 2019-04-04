c9d7bf6613a821a4e9f094d4612391b04590eadd
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-04-04 10:17:32 -0400

    cleaned up files

--- src/main/java/io/vertx/examples/tweetfilter/MainVerticle.java
+++ src/main/java/io/vertx/examples/tweetfilter/MainVerticle.java
@@ -1,11 +1,8 @@
 package io.vertx.examples.tweetfilter;
 
-import io.reactivex.Maybe;
 import io.reactivex.Observable;
-import io.reactivex.Single;
 import io.vertx.core.Future;
 import io.vertx.core.file.OpenOptions;
-import io.vertx.reactivex.RxHelper;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.buffer.Buffer;
 import io.vertx.reactivex.core.file.AsyncFile;
@@ -28,8 +25,6 @@ import java.util.List;
  */
 public class MainVerticle extends AbstractVerticle {
 
-  Observable prohibitedWords;
-
   ArrayList<String> badWords;
 
   @Override
@@ -43,17 +38,9 @@ public class MainVerticle extends AbstractVerticle {
       Observable<Buffer> observable = file.toObservable();
       observable.forEach(data -> {
         String[] splitData = data.toString().replace("\n", "").split(",");
-//        System.out.println("Read data: " + data.toString("UTF-8"));
         badWords.addAll(Arrays.asList(splitData));
       });
     });
-
-/*
-    prohibitedWords = Observable.fromArray(fileSystem.rxReadFile("Word_Filter-Sheet1.csv")
-                        .map(buffer -> buffer.toString().replace(",","").split(" "))
-                        .toObservable());
-    prohibitedWords.subscribe(badWords::add);
-*/
 
     Router baseRouter = Router.router(vertx);
     baseRouter.route("/").handler(this::indexHandler);
@@ -105,92 +92,5 @@ public class MainVerticle extends AbstractVerticle {
         .putHeader("Content-Type", "text/html")
         .end("Tweetfilter match!");
     }
-
-/*
-    System.out.println("text: " + statusInput);
-    prohibitedWords.forEach(System.out::println);
-
-
-    Observable<Boolean> allWordsOk = prohibitedWords.zipWith(statusWords, (prohibited, status) -> {
-      boolean retVal = Collections.disjoint(Arrays.asList(prohibited), Arrays.asList(status));
-      System.out.println("prohibited: " + prohibited.toString());
-      System.out.println("status: " + status);
-      System.out.println(retVal);
-      return retVal;
-    });
-
-    System.out.println(allWordsOk.subscribe());
-
-    List prohibitedArray = Arrays.asList(prohibitedWords);
-    prohibitedArray.forEach(System.out::println);
-
-    Single<Boolean> ok = statusWords.all(words -> {
-      boolean retVal = Collections.disjoint(Arrays.asList(prohibitedWords), Arrays.asList(words));
-      return retVal;
-    });
-
-    System.out.println(ok);
-
-    ok.subscribe(allClear ->{
-      if (allClear) {
-        routingContext.response()
-          .putHeader("Content-Type", "text/html")
-          .end("Tweetfilter all clear!");
-      }else{
-        routingContext.response()
-          .putHeader("Content-Type", "text/html")
-          .end("Tweetfilter match!");
-      }
-    });
-*/
-
-/*
-    if (match) {
-      routingContext.response()
-        .putHeader("Content-Type", "text/html")
-        .end("Tweetfilter");
-    }else{
-      routingContext.response()
-        .putHeader("Content-Type", "text/html")
-        .end("Tweetfilter");
-
-    }
-*/
-
-/*
-    prohibitedWords.contains(status).subscribe(res -> {
-      if (res) {
-        routingContext.response()
-          .putHeader("Content-Type", "text/html")
-          .end("Tweetfilter match!");
-      } else {
-        routingContext.response()
-          .putHeader("Content-Type", "text/html")
-          .end("Tweetfilter all clear!");
-      }
-    });
-*/
-  }
-
-/*
-    Observable words = Observable.fromArray(status.split(" "));
-
-    words.distinct().forEach(w -> {
-
-    });
-    Flowable<String> statusWords = Flowable.fromArray(status.split(" "));
-
-    statusWords.forEach(w -> System.out.println(w));
-
-    statusWords.forEach(w -> {
-      prohibitedMatch(w).doOnSuccess();
-    }).dispose();
-  }
-
-  public Maybe<Boolean> prohibitedMatch(String word) {
-    return prohibitedWords.contains(word).toMaybe();
-  }
-*/
-
 
 }
