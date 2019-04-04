735ae0c6d129b63604665e193f4ed1dabbd742d6
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-04-04 09:35:01 -0400

    marginally working

--- src/main/java/io/vertx/examples/tweetfilter/MainVerticle.java
+++ src/main/java/io/vertx/examples/tweetfilter/MainVerticle.java
@@ -1,12 +1,23 @@
 package io.vertx.examples.tweetfilter;
 
-import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.vertx.core.Future;
+import io.vertx.core.file.OpenOptions;
+import io.vertx.reactivex.RxHelper;
 import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.vertx.reactivex.core.file.AsyncFile;
 import io.vertx.reactivex.core.file.FileSystem;
+import io.vertx.reactivex.core.http.HttpServerResponse;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.handler.BodyHandler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -17,24 +28,169 @@ import java.util.List;
  */
 public class MainVerticle extends AbstractVerticle {
 
+  Observable prohibitedWords;
+
+  List<String> badWords;
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
 
+    badWords = new ArrayList<String>();
+
     FileSystem fileSystem = vertx.fileSystem();
-
-    Single<String> words =
-    fileSystem
-      .rxReadFile("Word_Filter-Sheet1.csv")
-      .map(buffer -> buffer.toString().replace(",",""));
-
-    words.contains("ass").subscribe(onNext -> {
-      System.out.println("match");
+    fileSystem.open("Word_Filter-Sheet1.csv", new OpenOptions(), res ->{
+      AsyncFile file = res.result();
+      Observable<Buffer> observable = file.toObservable();
+      observable.forEach(data -> {
+        String[] splitData = data.toString().split(",");
+//        System.out.println("Read data: " + data.toString("UTF-8"));
+        badWords.addAll(Arrays.asList(splitData));
+      });
     });
 
-    words.toFlowable().forEach(word -> System.out.println(word));
+/*
+    prohibitedWords = Observable.fromArray(fileSystem.rxReadFile("Word_Filter-Sheet1.csv")
+                        .map(buffer -> buffer.toString().replace(",","").split(" "))
+                        .toObservable());
+    prohibitedWords.subscribe();
+*/
 
-    startFuture.complete();
+    Router baseRouter = Router.router(vertx);
+    baseRouter.route("/").handler(this::indexHandler);
+
+    Router apiRouter = Router.router(vertx);
+    apiRouter.route("/*").handler(BodyHandler.create());
+    apiRouter.post("/censor").handler(this::censorshipHandler);
+
+    baseRouter.mountSubRouter("/api", apiRouter);
+
+    vertx
+      .createHttpServer()
+      .requestHandler(baseRouter::accept)
+      .listen(8081, result -> {
+        if (result.succeeded()) {
+          startFuture.complete();
+        } else {
+          startFuture.fail(result.cause());
+        }
+      });
+
   }
+
+  private void indexHandler(RoutingContext routingContext) {
+    HttpServerResponse response = routingContext.response();
+    response
+      .putHeader("Content-Type", "text/html")
+      .end("Tweetfilter");
+  }
+
+  private void censorshipHandler(RoutingContext routingContext) {
+
+    System.out.println("censorshipHandler");
+
+    String statusInput = routingContext
+      .getBodyAsJson()
+      .getString("text");
+
+    List<String> statusWords = Arrays.asList(statusInput.split(" "));
+
+    boolean allClear = Collections.disjoint(badWords, statusWords);
+
+    if (allClear) {
+      routingContext.response()
+        .putHeader("Content-Type", "text/html")
+        .end("Tweetfilter all clear!");
+    }else{
+      routingContext.response()
+        .putHeader("Content-Type", "text/html")
+        .end("Tweetfilter match!");
+    }
+
+/*
+    System.out.println("text: " + statusInput);
+    prohibitedWords.forEach(System.out::println);
+
+
+    Observable<Boolean> allWordsOk = prohibitedWords.zipWith(statusWords, (prohibited, status) -> {
+      boolean retVal = Collections.disjoint(Arrays.asList(prohibited), Arrays.asList(status));
+      System.out.println("prohibited: " + prohibited.toString());
+      System.out.println("status: " + status);
+      System.out.println(retVal);
+      return retVal;
+    });
+
+    System.out.println(allWordsOk.subscribe());
+
+    List prohibitedArray = Arrays.asList(prohibitedWords);
+    prohibitedArray.forEach(System.out::println);
+
+    Single<Boolean> ok = statusWords.all(words -> {
+      boolean retVal = Collections.disjoint(Arrays.asList(prohibitedWords), Arrays.asList(words));
+      return retVal;
+    });
+
+    System.out.println(ok);
+
+    ok.subscribe(allClear ->{
+      if (allClear) {
+        routingContext.response()
+          .putHeader("Content-Type", "text/html")
+          .end("Tweetfilter all clear!");
+      }else{
+        routingContext.response()
+          .putHeader("Content-Type", "text/html")
+          .end("Tweetfilter match!");
+      }
+    });
+*/
+
+/*
+    if (match) {
+      routingContext.response()
+        .putHeader("Content-Type", "text/html")
+        .end("Tweetfilter");
+    }else{
+      routingContext.response()
+        .putHeader("Content-Type", "text/html")
+        .end("Tweetfilter");
+
+    }
+*/
+
+/*
+    prohibitedWords.contains(status).subscribe(res -> {
+      if (res) {
+        routingContext.response()
+          .putHeader("Content-Type", "text/html")
+          .end("Tweetfilter match!");
+      } else {
+        routingContext.response()
+          .putHeader("Content-Type", "text/html")
+          .end("Tweetfilter all clear!");
+      }
+    });
+*/
+  }
+
+/*
+    Observable words = Observable.fromArray(status.split(" "));
+
+    words.distinct().forEach(w -> {
+
+    });
+    Flowable<String> statusWords = Flowable.fromArray(status.split(" "));
+
+    statusWords.forEach(w -> System.out.println(w));
+
+    statusWords.forEach(w -> {
+      prohibitedMatch(w).doOnSuccess();
+    }).dispose();
+  }
+
+  public Maybe<Boolean> prohibitedMatch(String word) {
+    return prohibitedWords.contains(word).toMaybe();
+  }
+*/
+
 
 }
--- src/test/java/io/vertx/examples/tweetfilter/TestMainVerticle.java
+++ src/test/java/io/vertx/examples/tweetfilter/TestMainVerticle.java
@@ -1,6 +1,10 @@
 package io.vertx.examples.tweetfilter;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
@@ -8,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -23,7 +28,7 @@ public class TestMainVerticle {
 
   @Test
   @DisplayName("Should start a Web Server on port 8888")
-  @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
+  @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
   void start_http_server(Vertx vertx, VertxTestContext testContext) throws Throwable {
     vertx.createHttpClient().getNow(8888, "localhost", "/", response -> testContext.verify(() -> {
       assertTrue(response.statusCode() == 200);
@@ -33,5 +38,33 @@ public class TestMainVerticle {
       });
     }));
   }
+
+  @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
+  @Test
+  @DisplayName("Should return all clear")
+  public void testThatWordIsCaught(Vertx vertx, VertxTestContext tc) {
+
+    WebClient webClient = WebClient.create(vertx);
+    Checkpoint deploymentCheckpoint = tc.checkpoint();
+    Checkpoint requestCheckpoint = tc.checkpoint();
+
+    vertx.deployVerticle(new MainVerticle(), tc.succeeding(id -> {
+
+      deploymentCheckpoint.flag();
+
+      webClient.post(8081, "localhost", "/api/censor")
+        .as(BodyCodec.string())
+        .sendJsonObject(new JsonObject().put("text", "cats are ass"), tc.succeeding(resp -> {
+          tc.verify(() -> {
+            assertThat(resp.statusCode()).isEqualTo(200);
+            assertThat(resp.body()).isNotEmpty();
+            assertThat(resp.body()).contains("Tweetfilter all clear!");
+            requestCheckpoint.flag();
+          });
+        }));
+    }));
+
+  }
+
 
 }
